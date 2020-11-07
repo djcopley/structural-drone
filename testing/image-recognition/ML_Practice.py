93624a514ae5d411290fe09cb4efff5364ad0586
Noah Levie <71532758+NLevie99@users.noreply.github.com>
2020-11-06 18:32:43 -0700

    Changed some features of the website and added several stock-drone images.

--- testing/image-recognition/ML_Practice.py
+++ testing/image-recognition/ML_Practice.py
@@ -20,7 +20,7 @@ print(tf.__version__)
 #
 # model.fit(xs, ys, epochs=500)
 #
-# print(model.predict([19]))
+# print(model.predict([20]))
 
 #################################################################################
 # Basic Computer Vision
@@ -67,7 +67,7 @@ print(tf.__version__)
 # # model.evaluate(test_images, test_labels)
 #
 # classifications = model.predict(test_images)
-# test_index = 69
+# test_index = 0
 #
 # print(classifications[test_index])
 # print(test_labels[test_index])
@@ -158,45 +158,45 @@ print(tf.__version__)
 #################################################################################
 # Build into combining convolution with DNN
 
-mnist = tf.keras.datasets.fashion_mnist
-(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
-training_images = training_images.reshape(60000, 28, 28, 1)  # Reshape for conv - 4d array
-training_images = training_images / 255.0
-test_images = test_images.reshape(10000, 28, 28, 1)  # Reshape for conv - 4d array
-test_images = test_images / 255.0
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),  # Conv Improvement
-    tf.keras.layers.MaxPooling2D(2, 2),  # Conv Improvement
-    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),  # Conv Improvement
-    tf.keras.layers.MaxPooling2D(2, 2),  # Conv Improvement
-
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dense(10, activation='softmax')
-])
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model.summary()
-model.fit(training_images, training_labels, epochs=1)
-test_loss, test_accuracy = model.evaluate(test_images, test_labels)
-print('Test loss: {}, Test accuracy: {}'.format(test_loss, test_accuracy * 100))
-
-# For showing the pooling results:
-f, axarr = plt.subplots(3, 4)
-FIRST_IMAGE = 0
-SECOND_IMAGE = 23
-THIRD_IMAGE = 28
-CONVOLUTION_NUMBER = 6
-
-layer_outputs = [layer.output for layer in model.layers]
-activation_model = tf.keras.models.Model(inputs=model.input, outputs=layer_outputs)
-for x in range(0, 4):
-    f1 = activation_model.predict(test_images[FIRST_IMAGE].reshape(1, 28, 28, 1))[x]
-    axarr[0, x].imshow(f1[0, :, :, CONVOLUTION_NUMBER], cmap='inferno')
-    axarr[0, x].grid(False)
-    f2 = activation_model.predict(test_images[SECOND_IMAGE].reshape(1, 28, 28, 1))[x]
-    axarr[1, x].imshow(f2[0, :, :, CONVOLUTION_NUMBER], cmap='inferno')
-    axarr[1, x].grid(False)
-    f3 = activation_model.predict(test_images[THIRD_IMAGE].reshape(1, 28, 28, 1))[x]
-    axarr[2, x].imshow(f3[0, :, :, CONVOLUTION_NUMBER], cmap='inferno')
-    axarr[2, x].grid(False)
-plt.show()
+# mnist = tf.keras.datasets.fashion_mnist
+# (training_images, training_labels), (test_images, test_labels) = mnist.load_data()
+# training_images = training_images.reshape(60000, 28, 28, 1)  # Reshape for conv - 4d array
+# training_images = training_images / 255.0
+# test_images = test_images.reshape(10000, 28, 28, 1)  # Reshape for conv - 4d array
+# test_images = test_images / 255.0
+# model = tf.keras.models.Sequential([
+#     tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),  # Conv Improvement
+#     tf.keras.layers.MaxPooling2D(2, 2),  # Conv Improvement
+#     tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),  # Conv Improvement
+#     tf.keras.layers.MaxPooling2D(2, 2),  # Conv Improvement
+#
+#     tf.keras.layers.Flatten(),
+#     tf.keras.layers.Dense(64, activation='relu'),
+#     tf.keras.layers.Dense(10, activation='softmax')
+# ])
+# model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+# model.summary()
+# model.fit(training_images, training_labels, epochs=1)
+# test_loss, test_accuracy = model.evaluate(test_images, test_labels)
+# print('Test loss: {}, Test accuracy: {}'.format(test_loss, test_accuracy * 100))
+#
+# # For showing the pooling results:
+# f, axarr = plt.subplots(3, 4)
+# FIRST_IMAGE = 0
+# SECOND_IMAGE = 23
+# THIRD_IMAGE = 28
+# CONVOLUTION_NUMBER = 6
+#
+# layer_outputs = [layer.output for layer in model.layers]
+# activation_model = tf.keras.models.Model(inputs=model.input, outputs=layer_outputs)
+# for x in range(0, 4):
+#     f1 = activation_model.predict(test_images[FIRST_IMAGE].reshape(1, 28, 28, 1))[x]
+#     axarr[0, x].imshow(f1[0, :, :, CONVOLUTION_NUMBER], cmap='inferno')
+#     axarr[0, x].grid(False)
+#     f2 = activation_model.predict(test_images[SECOND_IMAGE].reshape(1, 28, 28, 1))[x]
+#     axarr[1, x].imshow(f2[0, :, :, CONVOLUTION_NUMBER], cmap='inferno')
+#     axarr[1, x].grid(False)
+#     f3 = activation_model.predict(test_images[THIRD_IMAGE].reshape(1, 28, 28, 1))[x]
+#     axarr[2, x].imshow(f3[0, :, :, CONVOLUTION_NUMBER], cmap='inferno')
+#     axarr[2, x].grid(False)
+# plt.show()
